acaa3e3da6fb35141e712332691690f67fdcf192
AndreAlmeida <andredavi0903@gmail.com>
2025-09-13 02:49:13 -0400

    CSS mods

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
 import Introduction from "./pages/Introduction";
 import Home from "./pages/Home";
 import Authors from "./pages/Authors";
 import About from "./pages/About";
+import LogIn from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/introduction" element={<Introduction />} />
-        <Route path="/authors" element={<Authors />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/introduction" element={<Introduction />} />
+          <Route path="/authors" element={<Authors />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/singup" element={<SignUp />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   );
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "../style.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentYear = new Date();
+  return (
+    <footer className="footer">
+      <div className="footer-links">
+        <Link to="/about">Sobre</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/policts-privacy">Políticas de Privacidade</Link>
+      </div>
+      <div className="footer-credits">
+        <p>Densenvolvido por André Ameida ©{currentYear.getFullYear()}</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
