9ae95e378748a65d8b60620566e62e8efcf68a58
AndreAlmeida <andredavi0903@gmail.com>
2025-09-18 23:27:12 -0400

    Beginning of Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,34 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-page">
+      <header className="home-header">
+        <h1>Explore histórias</h1>
+        <p>|| BlogFics</p>
+      </header>
+      <hr className="line-between" />
+      <main className="home-main">
+        <section className="search-section">
+          <h2>Pesquisa</h2>
+          <input type="text" placeholder="Procurar por nome do autor ou obra" />
+          <button>Pesquisar</button>
+          <h3>Filtrar por:</h3>
+          <ul>
+            <li>Mais recentes</li>
+            <li>Mais comentados</li>
+            <li>Mais curtidos</li>
+          </ul>
+          <h3>Filtrar por gênero:</h3>
+          <ul>
+            <li>Comédia</li>
+            <li>Drama</li>
+            <li>Terror</li>
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
 };
 
 export default Home;
